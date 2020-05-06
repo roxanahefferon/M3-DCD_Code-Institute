ca038a168aebcaae50d6ffe3d5d1df7651f37546
roxanahefferon <roxana.hefferon@gmail.com>
2020-05-06 00:46:36 +0000

    base.html

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
